f3f0b786f9f9fdc8cc5f7b9303f57f8dc7b43b95
eiithel <ethel.marquer@gmail.com>
2016-07-18 11:45:56 -0400

    Implementation of preprocessor directives

--- main.cpp
+++ main.cpp
@@ -9,6 +9,16 @@
 #include <iostream>
 #include <istream>
 
+/*
+ * programme permettant de faire l'analyse statistique des delays rencontrés lors de chaque séance.
+ * Le programme se base sur un fichier .csv issu d'une session de capture wireshark.
+ * Le programme ajoute une ligne au fichier report.csv avec le Jitter (variance) et la moyenne des délais calculés.
+ *
+ * /!\ Pour utiliser le programme pour la première fois, mettre la directive de préprocesseur FIRST_TIME à "true".
+ *
+ * */
+
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -30,6 +40,10 @@ int main(int argc, char *argv[])
     QList<double> durations;
 
     durations = core.getDelayList();
+    double jitter = core.getJitter(durations);
+    double avgRTT = core.getAvgRTT(durations);
+
+#if DEBUG
 
     //affichage des delays calculés sur la console
     qDebug() << "retards calculees:\n";
@@ -38,13 +52,12 @@ int main(int argc, char *argv[])
         qDebug() << durations[i];
     }
 
-    double jitter = core.getJitter(durations);
-    double avgRTT = core.getAvgRTT(durations);
-
     qDebug() << "\nDelay moyen:";
     qDebug() << avgRTT;
-    qDebug() << "\n Jitter:";
+    qDebug() << "\nJitter:";
     qDebug() << jitter;
+
+#endif
 
     core.report(jitter, avgRTT);// save statistics to a .csv file
 
